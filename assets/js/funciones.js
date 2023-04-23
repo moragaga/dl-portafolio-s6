3526db4e4b3e6e2b5025d0a3f6a5ca89506ce836
Gerardo Moraga <moraga.ga@gmail.com>
2023-04-22 21:28:34 -0400

    Se agregan detalles a los pop up, se cambia información inicial

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,10 +1,22 @@
 const proyectos = {
-    "semana1": {"pagina":"https://moragaga.github.io/dl-primer-paso-cv-s1/", "imagen": "assets/img/semana1.jpg"},
-    "semana2": {"pagina":"https://moragaga.github.io/dl-chicstyle-s2/", "imagen": "assets/img/semana2.jpg"},
-    "semana3": {"pagina":"https://moragaga.github.io/dl-iguanas-s3/", "imagen": "assets/img/semana3.jpg"},
-    "semana4": {"pagina":"https://moragaga.github.io/dl-cuppon-s4/", "imagen": "assets/img/semana4.jpg"},
-    "semana5": {"pagina":"https://moragaga.github.io/dl-mini-cv-s5/", "imagen": "assets/img/semana5.jpg"},
-    "semana6": {"pagina":"https://moragaga.github.io/", "imagen": "assets/img/semana6.jpg"}
+    "semana1": {"pagina":"https://moragaga.github.io/dl-primer-paso-cv-s1/", 
+                "imagen": "assets/img/semana1.jpg",
+                "detalle": "Primer CV realizado unicamente con HTML."},
+    "semana2": {"pagina":"https://moragaga.github.io/dl-chicstyle-s2/", 
+                "imagen": "assets/img/semana2.jpg",
+                "detalle": "Primera Landing Page utilizando solo CSS para entregar una mejor visualización."},
+    "semana3": {"pagina":"https://moragaga.github.io/dl-iguanas-s3/", 
+                "imagen": "assets/img/semana3.jpg",
+                "detalle": "Construcción de Landing Page a partir de una maqueta utilizando solo CSS y Flex."},
+    "semana4": {"pagina":"https://moragaga.github.io/dl-cuppon-s4/", 
+                "imagen": "assets/img/semana4.jpg",
+                "detalle": "Construcción de Landing Page a partir de una maqueta utilizando Bootstrap y CSS. <br>(Adicionalmente cuenta con funcionalidades de JavaScript para dinamismo a los productos y al contacto)."},
+    "semana5": {"pagina":"https://moragaga.github.io/dl-mini-cv-s5/", 
+                "imagen": "assets/img/semana5.jpg",
+                "detalle": "Construcción de un CV a partir de un Fork realizando cambios con Bootstrap y CSS para poder validar conocimientos de Git. <br>(Adicionalmente cuenta con funcionalidades de JavaScript para dinamismo en el botón tecnología de los proyectos y al contacto)."},
+    "semana6": {"pagina":"https://moragaga.github.io/", "imagen": 
+                "assets/img/semana6.jpg",
+                "detalle": "Construcción de CV Final responsivo utilizando Bootstrap y CSS, se agrega enlace al portafolío actual. <br>(Adicionalmente cuenta con funcionalidades de JavaScript para dinamismo)."}
 }
 
 // Función que muestra imagen del proyecto y redirige a la página de dicho proyecto
@@ -21,11 +33,17 @@ document.querySelectorAll(".btn-popup").forEach((elemento) => {
         // Levanta pop up
         Swal.fire({
             html:`<div class="container-fluid">
-                    <div class="row text-center py-4">
+                    <div class="row text-center pt-4 pb-3">
                         <h1 class="color-celeste">Previsualización: ${titulo}</h1>
                     </div>
+                    <div class="row text-center text-white pb-2">
+                        <p class="p-0 m-0">${proyecto["detalle"]}</p>
+                    </div>
+                    <div class="row d-flex justify-content-center">
+                        <hr class="divisor w-75">
+                    </div>
                     <div class="text-center text-white">
-                        <p>Aquí una imagén del proyecto realizado, para ver la página pincha <b class="color-celeste">Ir a Página</b></p>
+                        <p class="detalle-pop-up">Aquí una imagén del proyecto realizado, para ver la página pincha <b class="color-celeste">Ir a Página</b></p>
                     </div>
                     <div class="row">
                         <img src="${proyecto["imagen"]}">
